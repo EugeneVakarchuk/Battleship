4992c791dfe719d120e1b6f56d9646b044c1f6bf
EugeneVakarchuk <eugenevakarchuk@gmail.com>
2022-10-28 00:01:22 +0300

    created battleground

--- src/comp/Battlefield.jsx
+++ src/comp/Battlefield.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import classes from '../styles.module.css';
 
+const Cell = (posY, posX) => {
+  return (
+    <div className={classes.cell}>
+    </div>
+  )
+}
+
+let matrix = [];
+
+for (let y = 0; y < 10; y++) {
+  matrix.push(new Array(0));
+  for (let x = 0; x < 10; x++) {
+    matrix[y].push(new Array(0))
+  }
+}
+
+
+
+console.log(matrix);
+
+
 const Battlefield = () => {
   return (
     <div className={classes.battlefield}>
-
+      <div>
+        {
+          matrix.map((column, indexY) =>
+            <div key={indexY} className={classes.column}>
+              {column.map((cell, indexX) =>
+                <Cell posY={indexY + 1} posX={indexX + 1} key={
+                  indexY.toString() + indexX.toString()
+                } />
+              )
+              }
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 };
